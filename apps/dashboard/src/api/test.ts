2b235b5dc73e899ea3f0cf69467eb7b83dab8dad
Egor Malyshev <egor.mlshv99@mail.ru>
2024-10-14 00:22:17 +0300

    add test page

---
+++ apps/dashboard/src/api/test.ts
@@ -0,0 +1,8 @@
+import {publicConfig} from "@/config";
+import {API_PATHS} from "@/enums/api";
+import {TestResponse} from "@/models/test";
+
+export const getTest = async (): Promise<TestResponse> => {
+    const response = await fetch(`${publicConfig.apiHost}/${API_PATHS.test}`);
+    return await response.json();
+};
---
+++ apps/dashboard/src/config.ts
@@ -0,0 +1,3 @@
+export const publicConfig = {
+  apiHost: process.env.NEXT_PUBLIC_API_HOST
+};
