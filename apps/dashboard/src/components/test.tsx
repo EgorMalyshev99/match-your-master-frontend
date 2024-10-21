1c28814c7fa04269aae5509f2b712c2f187ae729
Egor Malyshev <egor.mlshv99@mail.ru>
2024-10-21 20:19:59 +0300

    fix test

--- apps/dashboard/src/components/test.tsx
+++ apps/dashboard/src/components/test.tsx
@@ -1,23 +1,25 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { TestResponse } from "@/models/test";
-import { getTest } from "@/api/test";
 import Button from "@/components/button/Button";
+import { publicConfig } from "@/config";
+import { API_PATHS } from "@/enums/api";
 
 const Test = () => {
-  const [data, setData] = useState<TestResponse | null>(null);
+  const [data, setData] = useState<string | null>("{}");
   const [loading, setLoading] = useState<boolean>(false);
 
   const requestData = () => {
     setLoading(true);
-    getTest()
+    fetch(`${publicConfig.apiHost}/${API_PATHS.test}`)
+      .then((response) => response.json())
       .then((data) => {
+        console.log(data);
         setData(data);
       })
       .catch((error) => {
         console.error(error);
-        setData(null);
+        setData("{}");
       })
       .finally(() => {
         setLoading(false);
